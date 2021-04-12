e2d27930b051a8cd8715b5acc5c4c837f474b7ec
Vadim Koshelnik <vadimkoshelnik003@gmail.com>
2021-04-12 09:04:58 +0300

    To check

--- Data.cs
+++ Data.cs
@@ -13,27 +13,56 @@ namespace FoodDelivery
                                      IEnumerable<int> infantsIds,
                                      IEnumerable<int> childrenIds,
                                      IEnumerable<decimal> prices,
-                                     IEnumerable<string> currencies)
+                                     IEnumerable<string> currencies )
         {
            decimal fullPrice = default;
 
             // Your solution supposed to be here.
-            // IEnumerable<string> northheatherstreet = destinations.Where(destinations => destinations.Contains("North Heather Street"));
+           
+                     foreach (var etem in clients) { Console.WriteLine($"Звмовники:\t{etem}"); }
+                     Console.WriteLine("");
+                     foreach (var etem in destinations) { Console.WriteLine($"Адрес доставки:\t{etem}"); }
+                     Console.WriteLine("");
+                     foreach (var etem in prices) { Console.WriteLine($"Вартість достави без урахування знижки:\t{etem}");  }
+                     Console.WriteLine("");
+                     foreach (var etem in currencies) { Console.WriteLine($"Валюта якою розраховуватиметься замовник:\t{etem}"); }
+
             //int waynesreet = destinations.TakeWhile(destinations =>! destinations.Contains("North Heather Street")).Count();
             //var price = prices.ElementAt(waynesreet);
             //fullPrice = price + 10;
-
             var price = prices.ToArray();
-            var way= price[1]+10;
-            var nor = ((price[2] - 5.48m)*0.5m)* 1.19m;
-            var nor1 = ((price[3] - 5.48m)-4.41m)-1.98m;
-            var ta = (price[4] - 31.25m);
-            Console.WriteLine( fullPrice = price[0] + way + nor + nor1+ta ); 
+            var destination = destinations.ToArray();
+            var client = clients.ToArray();
+            var currencie = currencies.ToArray();
+            if ((price.Length == destination.Length) == (client.Length == currencie.Length))
+            {
+                var deliveryForJorgeHoffman = price[1] + 10;
+                var deliveryForAmiahSimmons = ((price[2] - 5.48m) * 0.5m) * 1.19m;
+                var deliveryForSariahBennett = ((price[3] - 5.48m) - 4.41m) - 1.98m;
+                var deliveryForXavierBowers = (price[4] - 31.25m);
+                fullPrice = (price[0] + deliveryForJorgeHoffman + deliveryForAmiahSimmons + deliveryForSariahBennett + deliveryForXavierBowers);
+                Console.WriteLine("");
+                Console.WriteLine("Сума яку заплатить перший замовник:\t" + price[0]);
+                Console.WriteLine("Сума яку заплатить другий замовник:\t" + deliveryForJorgeHoffman);
+                Console.WriteLine("Сума яку заплатить третій замовник:\t" + deliveryForAmiahSimmons);
+                Console.WriteLine("Сума яку заплатить четвертий замовник:\t" + deliveryForSariahBennett);
+                Console.WriteLine("Сума яку заплатить п'ятий замовник:\t" + deliveryForXavierBowers);
+                Console.WriteLine("");
+                Console.WriteLine($"Загальна сума доставки:{fullPrice}");
+                Console.WriteLine("");
+            }
+            else { Console.WriteLine("Дані не коректні"); }
+           
+
             return fullPrice;
         }
+
       
+
         public decimal InvokePriceCalculatiion()
         {
+            
+            
             var destinations = new[]
             {
                 "949 Fairfield Court, Madison Heights, MI 48071",
@@ -62,7 +91,9 @@ namespace FoodDelivery
             return GetFullPrice(destinations, clients, infantsIds, childrenIds, prices, currencies);
            
         }
-      
+
+        
+        
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 
+
 namespace FoodDelivery
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Data data = new Data();
-            data.InvokePriceCalculatiion();
-          
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Run();
             Console.ReadLine();
+        }
+
+        public static void Run()
+        {
+            Data data = new Data();
+            while (true)
+            {
+                Console.WriteLine("Доставка їжі Блиц-блиц без границ");
+                Console.WriteLine("");
+                data.InvokePriceCalculatiion(); break;
+                Console.ReadKey();   
+
+            }
+            
         }
     }
 }
