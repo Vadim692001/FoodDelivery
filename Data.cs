1338798dcde9333d38a44e33c936fce7cb330125
Vadim Koshelnik <vadimkoshelnik003@gmail.com>
2021-04-16 23:18:31 +0300

    Renaming constants

--- Data.cs
+++ Data.cs
@@ -7,18 +7,18 @@ namespace FoodDelivery
 {
   internal  class Data
     {
-       private const string wayneStreet = "Wayne Street";
-        private const string northHeatherStreet = "North Heather Street";
+       private const string WayneStreet = "Wayne Street";
+        private const string NorthHeatherStreet = "North Heather Street";
         private const char AddressSeparator = ' ';
 
-        private const decimal discountChildrens = 0.75m;
-        private const decimal discountInfants = 0.5m;
-        private const decimal neighborDiscount = 0.85m;
+        private const decimal DiscountChildrens = 0.75m;
+        private const decimal DiscountInfants = 0.5m;
+        private const decimal NeighborDiscountPercentage= 0.85m;
 
-        private  const decimal discountWayneStreet = 10;
-        private const decimal discountNorthHeatherStreet = 5.36m;
+        private  const decimal DiscountWayneStreet = 10;
+        private const decimal DiscountNorthHeatherStreet = 5.36m;
 
-        private const decimal euroExchange = 1.19m;
+        private const decimal EuroExchange = 1.19m;
         private decimal GetFullPrice(
                                      IEnumerable<string> destinations,
                                      IEnumerable<string> clients,
@@ -28,15 +28,12 @@ namespace FoodDelivery
                                      IEnumerable<string> currencies)
         {
             decimal fullPrice = default;
-            decimal sumPrice = default;
-           
-
-            foreach (var etem in clients) { Console.WriteLine($"Завмовники:\t{etem}"); }
+            decimal orderPrice = default;
+          
+            foreach (var item in clients) { Console.WriteLine($"Завмовники:\t{item}"); }
             Console.WriteLine("");
-            foreach (var etem in destinations) { Console.WriteLine($"Адрес доставки:\t{etem}"); }
+            foreach (var item in destinations) { Console.WriteLine($"Адрес доставки:\t{item}"); }
             Console.WriteLine("");
-
-
 
             if (!(prices.Count() == destinations.Count()) == (clients.Count() == currencies.Count()))
             {
@@ -44,42 +41,43 @@ namespace FoodDelivery
                 return -1;
             }
             Console.WriteLine("");
-            string pastaddres = default;
+
+            string pastAddress = default;
             foreach (var destination in destinations.Select((Value, Index) => (Value, Index)))
             {
-                 sumPrice = InitializeSumPrice(prices.ElementAt(destination.Index), sumPrice);
-               
-                 sumPrice = DeliveryWayneStreet(destination.Value, sumPrice);
-                 sumPrice = DeliveryNorthHeatherStreet(destination.Value, sumPrice);
-                 sumPrice = DiscountInfant(infantsIds, destination.Index, sumPrice);
-                 sumPrice = DiscountСhildren(childrenIds, destination.Index, sumPrice);
-                 sumPrice = NeighborDiscount(pastaddres, destination.Value, sumPrice);
+                orderPrice = InitializeSumPrice(prices.ElementAt(destination.Index), orderPrice);
+                orderPrice = DeliveryWayneStreet(destination.Value, orderPrice);
+                orderPrice = DeliveryNorthHeatherStreet(destination.Value, orderPrice);
+                orderPrice = DiscountInfant(infantsIds, destination.Index, orderPrice);
+                orderPrice = DiscountСhildren(childrenIds, destination.Index, orderPrice);
+                orderPrice = NeighborDiscount(pastAddress, destination.Value, orderPrice);
  
-              
-             
                 if (currencies.ElementAt(destination.Index).Contains("EUR"))
                 {
-                    sumPrice *= euroExchange;
+                    orderPrice *= EuroExchange;
                 }
-                pastaddres = destination.Value;
-                Console.WriteLine(clients.ElementAt(destination.Index) + ":" + sumPrice);
-                fullPrice += sumPrice;
+                pastAddress = destination.Value;
+                
+                Console.WriteLine(clients.ElementAt(destination.Index) + ":" + orderPrice);
+                fullPrice += orderPrice;
+
+
             }
        
             Console.WriteLine("Загальна сума доставки:\t"+ fullPrice);
             return fullPrice;
         }
-        private static decimal InitializeSumPrice(decimal price, decimal sumPrice)
+        private static decimal InitializeSumPrice(decimal price, decimal fullPrice)
         {
-            sumPrice = price;
+            fullPrice = price;
 
-            return sumPrice;
+            return fullPrice;
         }
         private static decimal DiscountInfant(IEnumerable<int> infants, int destination, decimal sumPrice)
         {
             if (infants.Contains(destination))
             {
-                sumPrice *= discountInfants;
+                sumPrice *= DiscountInfants;
             }
 
             return sumPrice;
@@ -88,25 +86,24 @@ namespace FoodDelivery
         {
             if (сhildrens.Contains(destination))
             {
-                sumPrice = sumPrice * discountChildrens;
+                sumPrice = sumPrice * DiscountChildrens;
             }
 
             return sumPrice;
         }
-
         private static decimal DeliveryWayneStreet(string destination, decimal sumPrice) 
         {
-            if (destination.Contains(wayneStreet))
+            if (destination.Contains(WayneStreet))
             {
-                sumPrice = sumPrice + discountWayneStreet;
+                sumPrice = sumPrice + DiscountWayneStreet;
             }
             return sumPrice;
         }
         private static decimal DeliveryNorthHeatherStreet(string destination, decimal sumPrice)
         {
-            if (destination.Contains(northHeatherStreet))
+            if (destination.Contains(NorthHeatherStreet))
             {
-                sumPrice = sumPrice - discountNorthHeatherStreet;
+                sumPrice = sumPrice - DiscountNorthHeatherStreet;
             }
             return sumPrice;
         }
@@ -115,7 +112,7 @@ namespace FoodDelivery
             
             if (NeighborDelivery(previousAddress, nextAddress))
             {
-                return sumPrice *= neighborDiscount;
+                return sumPrice *= NeighborDiscountPercentage;
             }
 
             return sumPrice;
@@ -152,11 +149,10 @@ namespace FoodDelivery
             var clients = new[]
             {
                 "Autumn Baldwin",
-               "Jorge Hoffman",
+                "Jorge Hoffman",
                 "Amiah Simmons",
                 "Sariah Bennett",
                 "Xavier Bowers",
-
             };
 
             var infantsIds = new[] { 2 };
@@ -165,13 +161,9 @@ namespace FoodDelivery
             var prices = new[] { 100, 25.23m,  58, 23.12m, 125 };
             var currencies = new[] { "USD", "USD", "EUR", "USD", "USD" };
             
-           
             return GetFullPrice(destinations, clients, infantsIds, childrenIds, prices, currencies);
            
         }
-
-        
-        
     }
 }
 
