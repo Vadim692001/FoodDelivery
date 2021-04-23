4f6577193d8c8f3c12fda5bac6ef1348dcd95aba
Vadim Koshelnik <vadimkoshelnik003@gmail.com>
2021-04-23 15:26:43 +0300

    Test

---
+++ ProgectInterface/OutputCreatedOrderProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static FoodDelivery.TestData.TestDataProduct;
+using static FoodDelivery.DataValidity.DataValidity;
+using static FoodDelivery.ProgectInterface.CreatedOrderProduct;
+;
+namespace FoodDelivery.ProgectInterface
+{
+ //public  class OutputCreatedOrderProduct
+ //   {
+
+ //       public static decimal OrderFormation()
+ //       {
+ //           decimal fullPrice = default;
+ //           decimal orderPrice = default;
+ //           Console.Write("Скільки замовлень ви хочете замовити:\t");
+ //           int.TryParse(Console.ReadLine(), out int number);
+ //           for (int i = 0; i < number; i++)
+ //           {
+ //               Console.Write("Ведіть номер товару:");
+ //               int.TryParse(Console.ReadLine(), out int numberProduct);
+ //               Console.Write("Ведіть кількість порцій:");
+ //               int.TryParse(Console.ReadLine(), out int numberPortion);
+ //               if (AvailabilityPortions(numberPortion, numberProduct) != true) { products.Clear(); break; }
+ //               orderPrice = OutputDataConsole(numberProduct, numberPortion);
+ //               fullPrice += orderPrice;
+ //           }
+ //           Console.WriteLine("Загальна сума:" + fullPrice);
+ //           products.Clear();
+ //           return fullPrice;
+ //       }
+ //   }
+}
